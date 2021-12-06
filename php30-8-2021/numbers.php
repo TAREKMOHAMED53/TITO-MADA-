502081d0547080afacc50bb54d2f0fcf5504195e
Tarek Mohamed <95379247+TAREKMOHAMED53@users.noreply.github.com>
2021-12-06 21:28:47 +0200

    6/12/2021
    6/12/2021

---
+++ php30-8-2021/numbers.php
@@ -0,0 +1,14 @@
+<?php
+$numbers=-1;
+
+
+if($numbers > 0){
+    echo 'positive';
+}elseif($numbers == 0){
+    echo 'zero';
+}else{
+    echo 'negative';
+}
+
+// localhost/foldername/filename.php
+//localhost/php30-8-2021/filename.php
---
+++ php30-8-2021/variables.php
@@ -0,0 +1,13 @@
+<?php
+$name='ahmed';   //string
+$number = 10 ;   //integer
+$number2 = 1.5 ;  //float
+$seen = true ;    //boolean
+
+// echo $name . $number . $number2 ;
+
+if($seen){
+    echo $name;
+}else{
+    echo $number;
+}
